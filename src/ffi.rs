4e02331d73353b3e006d416f53df1c50a0ee531c
Vladislav Nikonov <pacmancoder@gmail.com>
2019-12-08 18:01:15 +0200

    Added FreeRTOS task creation routine

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,5 +1,6 @@
 //! Module contains primitive type definitions for ffi with IDF framework
 
 pub type xtensa_int = isize;
+pub type xtensa_long = isize;
 pub type xtensa_unsigned_long = usize;
 pub type xtensa_void = core::ffi::c_void;
--- src/freertos.rs
+++ src/freertos.rs
@@ -5,6 +5,10 @@ type TickType_t = xtensa_unsigned_long;
 
 pub type QueueHandle_t = *mut xtensa_void;
 
+pub type TaskHandle_t = *mut xtensa_void;
+pub type TaskFunction_t = Option<unsafe extern "C" fn(arg1: *mut xtensa_void)>;
+
+
 extern "C" {
     pub fn xPortInIsrContext() -> BaseType_t;
     pub fn xPortGetTickRateHz() -> TickType_t;
@@ -12,4 +16,13 @@ extern "C" {
     pub fn vTaskDelay(xTicksToDelay: TickType_t);
 
     pub fn PendSV(request: xtensa_int);
+
+    pub fn xTaskCreate(
+        pxTaskCode: TaskFunction_t,
+        pcName: *const u8,
+        usStackDepth: u16,
+        pvParameters: *mut xtensa_void,
+        uxPriority: xtensa_unsigned_long,
+        pxCreatedTask: *mut TaskHandle_t,
+    ) -> xtensa_long;
 }
