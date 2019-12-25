3e91d2ab9211906bdde61763b09cd9fe57d66d38
Vladislav Nikonov <pacmancoder@gmail.com>
2019-12-25 11:59:50 +0200

    Reorganized error module

--- src/error.rs
+++ src/error.rs
@@ -2,8 +2,17 @@
 
 pub type esp_err_t = i32;
 pub const esp_err_t_ESP_OK: esp_err_t = 0;
-pub const esp_err_t_ESP_ERR_NO_MEM: esp_err_t = 257;
-pub const esp_err_t_ESP_ERR_INVALID_ARG: esp_err_t = 258;
-pub const esp_err_t_ESP_ERR_WIFI_CONN: esp_err_t = 12295;
-pub const esp_err_t_ESP_ERR_WIFI_NVS: esp_err_t = 12296;
-pub const esp_err_t_ESP_ERR_WIFI_PASSWORD: esp_err_t = 12299;
+pub const esp_err_t_ESP_FAIL: esp_err_t = -1;
+
+pub const esp_err_t_ESP_ERR_COMMON_BASE: esp_err_t = 0x100;
+pub const esp_err_t_ESP_ERR_NO_MEM: esp_err_t = esp_err_t_ESP_ERR_COMMON_BASE + 1;
+pub const esp_err_t_ESP_ERR_INVALID_ARG: esp_err_t = esp_err_t_ESP_ERR_COMMON_BASE + 2;
+
+pub const esp_err_t_ESP_ERR_WIFI_BASE: esp_err_t = 0x3000;
+pub const esp_err_t_ESP_ERR_WIFI_CONN: esp_err_t = esp_err_t_ESP_ERR_WIFI_BASE + 7;
+pub const esp_err_t_ESP_ERR_WIFI_NVS: esp_err_t = esp_err_t_ESP_ERR_WIFI_BASE + 8;
+pub const esp_err_t_ESP_ERR_WIFI_PASSWORD: esp_err_t = esp_err_t_ESP_ERR_WIFI_BASE + 11;
+
+const esp_err_t_ESP_ERR_NVS_BASE: esp_err_t = 0x1100;
+const esp_err_t_ESP_ERR_NVS_NOT_FOUND: esp_err_t = esp_err_t_ESP_ERR_NVS_BASE + 2;
+const esp_err_t_ESP_ERR_NVS_NO_FREE_PAGES: esp_err_t = esp_err_t_ESP_ERR_NVS_BASE + 13;
